018b45ac7ffba4f8472d028e5b52a4a0d08bd5f0
Damiao <tower_gamer@hotmail.com>
2020-09-13 18:41:49 -0300

    Socket.01

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+    socket.on("Welcome", (data) => {
+        console.log(data);
+    });
+
+    socket.on("word", (data) => {
+        console.log(data);
+    })
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+http.listen(3000, () => {
+    console.log("APP RODANDO!");
+})
